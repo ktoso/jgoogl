b94b471a1d3f1dc873628b9a1c398d61116e2a5d
Konrad Malawski <konrad.malawski@project13.pl>
2011-01-16 15:37:06 +0100

    First commit

---
+++ src/main/java/pl/project13/jgoogl/JGooGl.java
@@ -0,0 +1,17 @@
+package pl.project13.jgoogl;
+
+/**
+ * Date: 1/16/11
+ *
+ * @author Konrad Malawski
+ */
+public class JGooGl {
+
+  public JGooGl() {
+  }
+
+  public ShortGooGlResponse shorten(String url){
+
+    return new ShortGooGlResponse();
+  }
+}
