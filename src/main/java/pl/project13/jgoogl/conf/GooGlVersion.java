7cf443ebc5b3bfc0139e5e6d1a8dc9102e23e2bd
Konrad Malawski <konrad.malawski@project13.pl>
2011-02-28 18:04:25 +0100

    javadoc

--- src/main/java/pl/project13/jgoogl/conf/GooGlVersion.java
+++ src/main/java/pl/project13/jgoogl/conf/GooGlVersion.java
@@ -1,11 +1,14 @@
 package pl.project13.jgoogl.conf;
 
 /**
- * Date: 1/16/11
+ * Determine which GooGl version to use, they might have different URLs and API
  *
  * @author Konrad Malawski
  */
 public enum GooGlVersion {
+  /**
+   * First and currently only version of GooGl
+   */
   V1("https://www.googleapis.com/urlshortener/v1/url");
 
   public final String serviceUrl;
