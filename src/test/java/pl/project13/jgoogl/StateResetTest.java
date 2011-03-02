be3bfdc6ded85287eefa6fc1eb03b6a12a874225
Konrad Malawski <konrad.malawski@project13.pl>
2011-03-02 18:46:41 +0100

    adding mockito

---
+++ src/test/java/pl/project13/jgoogl/StateResetTest.java
@@ -0,0 +1,53 @@
+package pl.project13.jgoogl;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import pl.project13.jgoogl.conf.GooGlProjection;
+import pl.project13.jgoogl.response.v1.AnalyticsResponse;
+import pl.project13.jgoogl.response.v1.ExpandResponse;
+import pl.project13.jgoogl.response.v1.enums.GooGlStatus;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Date: 1/16/11
+ *
+ * @author Konrad Malawski
+ */
+public class StateResetTest {
+
+  Logger log = Logger.getLogger(getClass());
+
+  JGooGl jGooGl = JGooGl.withoutKey();
+
+  // test data
+  String longUrl          = "http://www.project13.pl/"; // note the trailing slash, Google will add it in their response anyways
+  String shortenedLongUrl = "http://goo.gl/ZA9Yx"; // the above should be shortened to this url
+
+  @Test
+  public void shouldExpandProperly() throws Exception {
+    AnalyticsResponse analyticsResponse = jGooGl.analyticsFor(shortenedLongUrl);
+
+    log.info("Deserialized response: " + analyticsResponse);
+
+    assertThat(analyticsResponse).isNotNull();
+    assertThat(analyticsResponse.hasErrors()).isFalse();
+    assertThat(analyticsResponse.getShortUrl()).isEqualTo(shortenedLongUrl);
+    assertThat(analyticsResponse.getStatus()).isEqualTo(GooGlStatus.OK);
+    assertThat(analyticsResponse.getLongUrl()).isEqualTo(longUrl);
+    assertThat(analyticsResponse.getKind()).isEqualTo(EXPECTED_SHORTENER_KIND);
+  }
+
+  @Test
+  public void queryFluentAnalytics() throws Exception {
+    ExpandResponse expand = jGooGl.withAnalytics(GooGlProjection.ANALYTICS_FULL).expand(shortenedLongUrl);
+
+    log.info(expand);
+
+    assertThat(expand).isInstanceOf(AnalyticsResponse.class);
+    AnalyticsResponse analyticsResponse = (AnalyticsResponse) expand;
+
+    assertThat(analyticsResponse.getAnalytics()).isNotNull();
+    assertThat(analyticsResponse.getLongUrl()).isEqualTo(longUrl);
+  }
+}
